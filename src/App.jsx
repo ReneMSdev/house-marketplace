13ff0c8c80e4c546698dfcbe9acd36714d56af7d
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-07 22:09:17 +0700

    User Sign In

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           />
           <Route
             path="/profile"
-            element={<SignIn />}
+            element={<Profile />}
           />
           <Route
             path="/sign-in"
