b275f5df27c5be4492690e2412977982ca08d07e
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-09 13:18:11 +0700

    Offers Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
                 pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'
               }
             >
-              Offer
+              Offers
             </p>
           </li>
           <li
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,4 +1,80 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
+
 const Offers = () => {
-  return <h1>Offers</h1>
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get a reference
+        const listingRef = collection(db, 'listings')
+
+        // Create a query
+        const q = query(
+          listingRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnapshot = await getDocs(q)
+
+        const listings = []
+
+        querySnapshot.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          })
+        })
+
+        setListings(listings)
+        setLoading(false)
+        // eslint-disable-next-line no-unused-vars
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+    }
+
+    fetchListings()
+  }, [])
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
+    </div>
+  )
 }
 export default Offers
