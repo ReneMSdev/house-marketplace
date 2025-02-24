0b965b5e07007ed562e2f4dfca85d74f168b8dde
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-24 16:43:36 +0700

    fetch single listing

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -53,6 +54,10 @@ function App() {
           <Route
             path="/forgot-password"
             element={<ForgotPassword />}
+          />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
           />
         </Routes>
         <Navbar />
