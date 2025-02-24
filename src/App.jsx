b30d5dc8ef055c9fba936cc61b6fafd2d15d5fa1
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-24 19:37:05 +0700

    Contact Landlord Page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -58,6 +59,10 @@ function App() {
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
+          />
+          <Route
+            path="/contact/:landlordId"
+            element={<Contact />}
           />
         </Routes>
         <Navbar />
